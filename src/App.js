69dd1b49a673203dede7284f683ba0d7c5c6dd67
coolwis <wisdombrave@gmail.com>
2020-02-16 21:40:07 +0900

    customer js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2> Let's develop ManageMent System! </h2>
-    </div>
-  );
+import Customer from './components/Customer'
+
+const customers =[
+  {
+  'id': 1, 
+  'image': 'https://placeimg.com/64/64/1',
+  'name':'1 hong',
+  'birthday':'78500',
+  'gender': 'manman',
+  'job':'programmer'
+},
+{
+  'id': 2, 
+  'image': 'https://placeimg.com/64/64/2',
+  'name':'2hong',
+  'birthday':'78500',
+  'gender': 'manman',
+  'job':'programmer'
+}
+,{
+  'id': 3, 
+  'image': 'https://placeimg.com/64/64/3',
+  'name':'3hong',
+  'birthday':'78500',
+  'gender': 'manman',
+  'job':'programmer'
+}
+,{
+  'id': 4, 
+  'image': 'https://placeimg.com/64/64/4',
+  'name':'4hong',
+  'birthday':'78500',
+  'gender': 'manman',
+  'job':'programmer'
+}
+]
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {
+          customers.map(c=> {
+            return (
+              <Customer 
+                key ={c.id}
+                id ={c.id}
+                image ={c.image}
+                name ={c.name}
+                birthday ={c.birthday}
+                gender ={c.gender}
+                job ={c.job}    
+                /> 
+            );
+                      }            
+                    )
+        }
+    
+      </div>
+    )   
+  }
 }
 
 export default App;
