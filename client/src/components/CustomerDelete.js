cc9e2b984ba8bd9851b6396a8578dd50046a5044
coolwis <wisdombrave@gmail.com>
2020-02-19 19:16:52 +0900

    add Dialog material design

--- client/src/components/CustomerDelete.js
+++ client/src/components/CustomerDelete.js
@@ -1,6 +1,23 @@
 import React from 'react';
 
+
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+// import {withStyles} from '@material-ui/core/styles';
+
+
 class CustomerDelete extends React.Component{
+    constructor(props){
+        super(props);
+        this.state ={             
+            open: false
+        }
+    }
+
 deleteCustomer(id){
     const url='/api/customers/' +id;
     fetch(url,{
@@ -9,10 +26,42 @@ deleteCustomer(id){
         this.props.stateRefresh();
 }//end deleteCustomer
 
+handleClickOpen= () => {
+    this.setState({
+        open:true
+    });
+}
+
+handleClose= () => {
+    this.setState(
+        {
+            open: false
+        }
+    )
+}
+
+
     render() {
         return (
-            <button onClick={(e) =>{this.deleteCustomer(this.props.id)} }>삭제</button>
-        )
+            <div>
+            <Button variant="contained" color="secondary" onClick={this.handleClickOpen }>삭제</Button>
+       
+            <Dialog open={this.state.open} onClose={this.handleClose}>
+                <DialogTitle onClose={this.handleClose}>삭제 확인</DialogTitle>
+
+                <DialogContent>
+                    <Typography gutterBottom>
+선택한 고객 정보가 삭제됩니다.
+                    </Typography>
+                </DialogContent>
+                <DialogActions>
+<Button variant="contained" color="primary" onClick={(e)=>{this.deleteCustomer(this.props.id)}} >Delete</Button>
+<Button variant="outlined" color="primary" onClick={this.handleClose} >Close</Button>
+
+                </DialogActions>
+            </Dialog>
+            </div>
+            )
     }
 } //end class
 
